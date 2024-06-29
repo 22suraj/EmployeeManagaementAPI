a6f4547c6329d71855d9d1e985186ad5e781bc00
22suraj <surajjadhav525k@gmail.com>
2024-06-29 17:05:43 +0530

    Initial commit

---
+++ Data/EmployeeDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeManagementAPI.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementAPI.Data
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
